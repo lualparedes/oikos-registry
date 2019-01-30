f475c415e650b9fb199caf0480a31efc104ee5ca
Luis Paredes <lualps94@gmail.com>
2019-01-30 18:06:25 -0400

    Refactored test suite for Editor and added a new test

--- src/components/editor/editor.component.test.js
+++ src/components/editor/editor.component.test.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Editor from './editor.component';
-
+import ModalSelect from '../modal-select/modal-select.component';
+import TestRenderer from 'react-test-renderer';
 import { createStore } from 'redux';
 import { reducer } from '../../reducers';
 
-const store = createStore(reducer);
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Editor store={ store } />, div);
+describe('Editor component', () => {
+
+  const store = createStore(reducer);
+
+  it('renders without crashing using ReactDOM', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Editor store={ store } />, div);
+  });
+
+  it('has one ModalSelect', () => {
+    const testRenderer = TestRenderer.create(<Editor store={ store } />);
+    const testInstance = testRenderer.root;
+    expect(testInstance.findAllByType(ModalSelect).length).toBe(1);
+  });
 });
