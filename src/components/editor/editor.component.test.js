4e0fb37cf841557f526c6c35ddaa85d84b813e2a
Luis Paredes <lualps94@gmail.com>
2019-01-30 17:40:18 -0400

    Implemented basic test for Editor

--- src/components/editor/editor.component.test.js
+++ src/components/editor/editor.component.test.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Editor from './editor.component';
 
+import { createStore } from 'redux';
+import { reducer } from '../../reducers';
+
+const store = createStore(reducer);
+
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Editor />, div);
+  const div = document.createElement('div');
+  ReactDOM.render(<Editor store={ store } />, div);
 });
